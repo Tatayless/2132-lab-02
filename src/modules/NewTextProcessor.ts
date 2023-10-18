3e4593be96f6d8b54d34be4c8c8b0f4b7c9293cc
Anthony John Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-10-19 00:26:45 +0800

    feat(index): deprecated adapter pattern

--- src/modules/NewTextProcessor.ts
+++ src/modules/NewTextProcessor.ts
@@ -1,20 +1,19 @@
 export class NewTextProcessor {
-    processNewText(text: { [key: string]: string }) {
-        const formattedData: { [key: string]: string } = {
-            "promo_code": "",
-            "promo_number": "",
-            "originating_number": "",
-            "destination_number": "",
-            "date": ""
-        };
+  processNewText(text: { [key: string]: string }) {
+    const formattedData: { [key: string]: string } = {
+      promo_code: "",
+      promo_number: "",
+      originating_number: "",
+      destination_number: "",
+      date: "",
+    };
 
-        for (const key of Object.keys(text)) {
-            formattedData[key] = text[key];
-        }
-
-        Object.entries(text).forEach(([key, value]) => {
-            console.log(`${key}: ${value}`);
-        });
-        
+    for (const key of Object.keys(text)) {
+      formattedData[key] = text[key];
     }
+
+    Object.entries(text).forEach(([key, value]) => {
+      console.log(`${key}: ${value}`);
+    });
+  }
 }
