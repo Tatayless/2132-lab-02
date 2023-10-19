4babe7a66d71b6fc046f38ceefe015429e57d8cb
Anthony John Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2023-10-19 10:50:52 +0800

    feat(index): can now encrypt new text

--- src/modules/TextProcessorDecorator.ts
+++ src/modules/TextProcessorDecorator.ts
@@ -1,5 +1,64 @@
-import { LegacyTextProcessor, NewTextProcessor, TextProcessorAdapter } from "./TextProcessor";
+import {
+  TextProcessor,
+  LegacyTextProcessor,
+  NewTextProcessor,
+} from "./TextProcessor";
+import { CaesarCipher } from "./DecoratorCipher";
 
-export class EncryptedNewTextProcessor {
-    
+class TextProcessorEncryptor implements TextProcessor {
+  private processor: TextProcessor;
+
+  constructor(processor: TextProcessor) {
+    this.processor = processor;
+  }
+
+  processText(text: { [key: string]: string }) {
+    this.processor.processText(text);
+
+    if (text === undefined) {
+      text = {};
+      return text;
+    } else {
+      Object.entries(text).forEach(([key, value]) => {
+        text[key] = CaesarCipher.encrypt(value);
+      });
+      return text;
+    }
+  }
 }
+
+class TextProcessorDecryptor implements TextProcessor {
+  private processor: TextProcessor;
+
+  constructor(processor: TextProcessor) {
+    this.processor = processor;
+  }
+
+  processText(text: { [key: string]: string }): { [key: string]: string } {
+    this.processor.processText(text);
+
+    if (text === undefined) {
+      text = {};
+      return text;
+    } else {
+      Object.entries(text).forEach(([key, value]) => {
+        text[key] = CaesarCipher.decrypt(value);
+      });
+      return text;
+    }
+  }
+}
+
+const encryptor = new TextProcessorEncryptor(new NewTextProcessor());
+const decryptor = new TextProcessorDecryptor(new NewTextProcessor());
+const textData = {
+  promo_code: "ABC123",
+  promo_number: "9876543210",
+  originating_number: "1234567890",
+  destination_number: "0987654321",
+  date: "20231019",
+};
+let newEncryptedText: { [key: string]: string } = encryptor.processText(textData);
+console.log(newEncryptedText);
+console.log(decryptor.processText(newEncryptedText));
+
